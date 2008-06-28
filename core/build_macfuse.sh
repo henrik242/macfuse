804214468881f83029a7918032bc155100255064
singh <singh@febdc8c4-1326-0410-abd6-27cb15717650>
2008-06-28 01:10:49 +0000

    1.7-interim: build script updates.

--- core/build_macfuse.sh
+++ core/build_macfuse.sh
@@ -2,10 +2,25 @@
 # Copyright (C) 2006-2007 Amit Singh. All Rights Reserved.
 #
 
+CUT=/usr/bin/cut
+DIRNAME=/usr/bin/dirname
+RM=/bin/rm
 UNAME=/usr/bin/uname
 
 os_name=`$UNAME -s`
 os_codename="Unknown"
+
+is_absolute_path=`echo "$0" | $CUT -c1`
+if [ "$is_absolute_path" = "/" ]
+then
+    macfuse_dir="`$DIRNAME $0`/.."
+else
+    macfuse_dir="`pwd`/`$DIRNAME $0`/.."
+fi
+pushd . > /dev/null
+cd "$macfuse_dir" || exit 1
+macfuse_dir=`pwd`
+popd > /dev/null
 
 os_release=`$UNAME -r`
 if [ "$1" != "" ]
@@ -22,12 +37,26 @@ then
 fi
 
 case "$os_release" in
+  0)
+      sudo true # Need root password to delete previous builds
+      if [ $? -ne 0 ]
+      then
+          echo "Administrator password is required to clean up MacFUSE builds."
+          exit 1
+      fi
+      echo "Cleaning up any previous MacFUSE builds"
+      sudo $RM -rf "$macfuse_dir"/core/10.4/fusefs/build/
+      sudo $RM -rf "$macfuse_dir"/core/10.5/fusefs/build/
+      sudo $RM -rf "$macfuse_dir"/core/sdk-objc/build/
+      sudo $RM -rf "/tmp/macfuse-core-10.*-*"
+      exit 0
+  ;;
   8*)
-      src_dir="./10.4/fusefs/"
+      src_dir="$macfuse_dir/core/10.4/fusefs/"
       os_codename="Tiger"
   ;;
   9*)
-      src_dir="./10.5/fusefs/"
+      src_dir="$macfuse_dir/core/10.5/fusefs/"
       os_codename="Leopard"
   ;;
   *)
